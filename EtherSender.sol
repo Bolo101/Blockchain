ca6bd0a6d5689c04251a5594edd1bc59c7304bbf
Bolo101 <iglesias.guillaumedu64@gmail.com>
2023-06-11 18:55:07 +0200

    Comments translated

--- EtherSender.sol
+++ EtherSender.sol
@@ -4,12 +4,12 @@ contract EtherSender{
     address lastSender;
     uint balance;
 
-                                //payable indique que l'on transfère des fonds
-                                // external indique que le contrat est appelé uniquement depuis l'exterieur
-                                //public peut etre appele depuis l'interieur ou l'exterieur
+                                //payable means we transfer funds
+                                // external means the contract is called from the exterior
+                                //public can be called from both inside and outside
     receive() external payable{ 
-        lastSender = msg.sender; //msg.sender contient l'adresse de la dernière personne qui a envoyé de l'argent
-        balance = balance + msg.value; //msg.value est l'argent envoyé sur le contrat intelligent
+        lastSender = msg.sender; //msg.sender contains the address of the last person who sent money to the contract
+        balance = balance + msg.value; //msg.value is the money sent on the intelligent contract
     } 
 
     function getLastSender() public view returns(address){
