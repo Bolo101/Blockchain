a2dbde93b2bd80b6c5fd0b2ca24e1ae74a29f00f
Bolo101 <iglesias.guillaumedu64@gmail.com>
2023-06-11 16:35:42 +0200

    StructureOfControl updated

--- StructureOfControl.sol
+++ StructureOfControl.sol
@@ -5,7 +5,8 @@ contract StructureOfControl{
     function setNombre(uint _nombre)public{
         if(_nombre ==10){
             revert('le nombre ne peut pas etre egal a 10');//revert stop the function and give back remaining gas
-        }
+	//revert is when condition is matched in this case	        
+	}
         nombre = _nombre;
     }
     function getNombre() public view returns(uint){
@@ -14,7 +15,8 @@ contract StructureOfControl{
     /**
     function setNombre(uint _nombre) public{
         require(_nombre !=10,"nombre ne peut pas etre egal a 10");//also give back remaining gas
-        //validate execution if the user is owner or not. Validate transmitted user's data
+	//require check if the condition is matched, it does the validation        
+	//validate execution if the user is owner or not. Validate transmitted user's data
     }    
     //can also use 'assert' for less important issues such as a division by 0. Do not give gas back
     */
