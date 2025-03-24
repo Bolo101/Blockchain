647f018b5e1f7791920b8c05b6bd08b8d0b63fae
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-03-24 06:15:36 +0100

    Formatted code

--- ultimateLearning/foundryFundamentals/foundryFundMe/forge/src/FundMe.sol
+++ ultimateLearning/foundryFundamentals/foundryFundMe/forge/src/FundMe.sol
@@ -6,21 +6,23 @@ import {PriceConverter} from "./PriceConverter.sol";
 
 error NotOwner();
 
-contract FundMe{
+contract FundMe {
     using PriceConverter for uint256; //can use every function from the library as a method on any uint256 variable in our main contract code
-    
-    uint256 public constant MINIMUM_USD = 5e18;   //add constant to limit gas fees because it no longer takes a storage spot. Only if the value is not modified
+
+    uint256 public constant MINIMUM_USD = 5e18; //add constant to limit gas fees because it no longer takes a storage spot. Only if the value is not modified
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
     address public immutable i_owner; //immutable as this value is not going to be modified. More gas efficient
 
-    receive() external payable { //in case fund function is not used correctly
+    receive() external payable {
+        //in case fund function is not used correctly
         fund();
     }
-                                     
-    fallback() external payable {//in case fund function is not used correctly
+
+    fallback() external payable {
+        //in case fund function is not used correctly
         fund();
     }
 
@@ -28,42 +30,43 @@ contract FundMe{
         i_owner = msg.sender;
     }
 
-    function fund() public payable{//payable means we can transfer funds
+    function fund() public payable {
+        //payable means we can transfer funds
         //allow  users to send funds
         //set a minimum amount to pay
-        require(msg.value.getConversionRate() >= MINIMUM_USD,"Not enough eth to send"); // at leats one ether
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Not enough eth to send"); // at leats one ether
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public onlyOwner{
-        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
-        funders = new address[](0);//reset funder array with initial length of zero
+        funders = new address[](0); //reset funder array with initial length of zero
         (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
-        revert();   
+        revert();
     }
 
-    modifier onlyOwner(){
+    modifier onlyOwner() {
         //require(msg.sender == i_owner, "Not the owner");
         // to be more gas efficient we can use custom error instead of require
-        if(msg.sender != i_owner) { revert NotOwner();}
+        if (msg.sender != i_owner) revert NotOwner();
         _;
     }
     // msg.sender = address and payable(msg;sender) = payable address
-//transfer  simplest one. (msg.sender.transfert(address(this).balance) this is the contract
-// transfer throw an error if gas are over 2300 for the operation and operation is reverted
-//send returns a boolean if gas are over 2300, only revert if we add a require
-// bool sendSucess = payable(msg.sender).send(address(this).balance);
-// require(sendSuccess, "Send failed");
-//call
-// payable(msg.sender).call{value: address(this).balance)("") call("") is for calling a function, we live it empty here. 
-// call returns two variables
-// (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}("")
-//dataReturned is useful if we call a function
-// (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}("")
-// require(callSuccess, "Call failed");
+    //transfer  simplest one. (msg.sender.transfert(address(this).balance) this is the contract
+    // transfer throw an error if gas are over 2300 for the operation and operation is reverted
+    //send returns a boolean if gas are over 2300, only revert if we add a require
+    // bool sendSucess = payable(msg.sender).send(address(this).balance);
+    // require(sendSuccess, "Send failed");
+    //call
+    // payable(msg.sender).call{value: address(this).balance)("") call("") is for calling a function, we live it empty here.
+    // call returns two variables
+    // (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}("")
+    //dataReturned is useful if we call a function
+    // (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}("")
+    // require(callSuccess, "Call failed");
 }
