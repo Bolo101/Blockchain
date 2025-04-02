b80f05cf672de730c36ad99eb0469861a1521798
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-04-02 06:57:52 +0200

    Formge formatted

--- ultimateLearning/foundryFundamentals/foundryFundMe/forge/src/PriceConverter.sol
+++ ultimateLearning/foundryFundamentals/foundryFundMe/forge/src/PriceConverter.sol
@@ -6,19 +6,14 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/Ag
 // Why is this a library and not abstract?
 // Why not an interface?
 library PriceConverter {
-    function getPrice(
-        AggregatorV3Interface priceFeed
-    ) internal view returns (uint256) {
+    function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
         // Sepolia ETH / USD Address
         // https://docs.chain.link/data-feeds/price-feeds/addresses
-        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        (, int256 answer,,,) = priceFeed.latestRoundData();
         return uint256(answer * 10000000000);
     }
 
-    function getConversionRate(
-        uint256 ethAmount,
-        AggregatorV3Interface priceFeed
-    ) internal view returns (uint256) {
+    function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
         uint256 ethPrice = getPrice(priceFeed);
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
         return ethAmountInUsd;
