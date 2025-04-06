ad6aa8fd27987a3119922aa26a7ee5bc653530fd
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-04-06 20:01:36 +0200

    Added eth mainnet price feed support

--- ultimateLearning/foundryFundamentals/foundryFundMe/forge/script/HelperConfig.s.sol
+++ ultimateLearning/foundryFundamentals/foundryFundMe/forge/script/HelperConfig.s.sol
@@ -15,8 +15,10 @@ contract HelperConfig {
 
     constructor() {
         if (block.chainid == 11155111) {
-            //chainid from
+            //chainid from chainlist.org
             activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
         } else {
             activeNetworkConfig = getAnvilEthConfig();
         }
@@ -27,6 +29,13 @@ contract HelperConfig {
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         }); //optional {}
         return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        }); //optional {}
+        return ethConfig;
     }
 
     function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
