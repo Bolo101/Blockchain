aaf15d4520bb5c1b097580a736d715e160fa73e8
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-04-30 06:43:45 +0200

    Custom error

--- ultimateLearning/foundryFundamentals/foundryLottery/src/Raffle.sol
+++ ultimateLearning/foundryFundamentals/foundryLottery/src/Raffle.sol
@@ -29,21 +29,28 @@ pragma solidity 0.8.19;
  * @notice This contract is for creating a sample raffle
  * @dev Implements Chainlink VRFv2.5
  */
-
 contract Raffle {
+    error SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        //require(msg.value >= i_entranceFee, "Not enough ETH sent!")
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle()); since 0.8.26
+        if (msg.value < i_entranceFee) {
+            revert SendMoreToEnterRaffle();
+        }
+    }
 
     function pickWinner() public {}
 
-    /** 
-    Getters 
-    */
+    /**
+     * Getters
+     */
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
