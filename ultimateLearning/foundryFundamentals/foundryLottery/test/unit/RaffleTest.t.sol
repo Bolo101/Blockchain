b0b6b30acb81b5f88e05558eba55c135fd8feca5
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-06-29 13:10:01 +0200

    Added more test

--- ultimateLearning/foundryFundamentals/foundryLottery/test/unit/RaffleTest.t.sol
+++ ultimateLearning/foundryFundamentals/foundryLottery/test/unit/RaffleTest.t.sol
@@ -109,4 +109,41 @@ contract RaffleTest is Test {
 
     // testCheckUpkeepReturnsFalseIfEnoughTimeHasntPassed
     // testCheckUpkeepReturnsTrueWhenParametersAreGood
+
+    /*//////////////////////////////////////////////////////////////
+                             PERFORM UPKEEP
+    //////////////////////////////////////////////////////////////*/
+    function testPerformUpkeepCanOnlyRunIfCheckUpkeepIsTrue() public {
+        // Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+
+        // Act/Assert
+        raffle.performUpkeep("");
+    }
+
+    function testPerformUpkeepIfCheckUpkeepIsFalse() public {
+        // Arrange
+        uint256 currentBalance = 0;
+        uint256 numPlayers = 0;
+        Raffle.RaffleState rState = raffle.getRaffleState();
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        currentBalance += entranceFee;
+        numPlayers = 1;
+
+        // Act/assert
+        // Expect custom error with parameters
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Raffle.Raffle__UpkeepNotNeeded.selector,
+                currentBalance,
+                numPlayers,
+                rState
+            )
+        );
+        raffle.performUpkeep("");
+    }
 }
