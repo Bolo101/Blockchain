8f8a4434b8906be2007c619dba7c4e193ff3cadb
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-07-02 08:36:11 +0200

    fulfillRandomWords test

--- ultimateLearning/foundryFundamentals/foundryLottery/test/unit/RaffleTest.t.sol
+++ ultimateLearning/foundryFundamentals/foundryLottery/test/unit/RaffleTest.t.sol
@@ -193,4 +193,52 @@ contract RaffleTest is Test {
             address(raffle)
         );
     }
+
+    function testFulfillrandomWordsPicksAWinnerResetsAndSendsMoney()
+        public
+        raffleEntered
+    {
+        // Arrange
+        uint256 additionalEntrants = 3; // 4 total
+        uint256 startingIndex = 1;
+        address expectedWinner = address(1);
+
+        for (
+            uint i = startingIndex;
+            i < startingIndex + additionalEntrants;
+            i++
+        ) {
+            address newPlayer = address(uint160(i)); // converts number into an address
+            hoax(newPlayer, 1 ether);
+            raffle.enterRaffle{value: entranceFee}();
+        }
+        uint256 startingTimeStamp = raffle.getLastTimeStamp();
+        uint256 winnerStartingBalance = expectedWinner.balance;
+
+        // Act
+        vm.recordLogs();
+        raffle.performUpkeep("");
+        Vm.Log[] memory entries = vm.getRecordedLogs(); // inlogs everetyhing is stored as byte32.
+        // We want to get topic requestId from logs
+        //entries[0] will be event from the vrfCoordinator itself. Remember our event is redundant
+        bytes32 requestId = entries[1].topics[1]; //topic[0] is a reserved space
+
+        // Following code will call fulfillRandomWords of raffle contract from mock
+        VRFCoordinatorV2_5Mock(vrfCoordinator).fulfillRandomWords(
+            uint256(requestId),
+            address(raffle)
+        );
+
+        // Assert
+        address recentWinner = raffle.getRecentWinner();
+        Raffle.RaffleState raffleState = raffle.getRaffleState();
+        uint256 winnerBalance = recentWinner.balance;
+        uint256 endingTimeStamp = raffle.getLastTimeStamp();
+        uint256 prize = entranceFee * (additionalEntrants + 1);
+
+        assert(recentWinner == expectedWinner);
+        assert(uint256(raffleState) == 0);
+        assert(winnerBalance == winnerStartingBalance + prize);
+        assert(endingTimeStamp > startingTimeStamp);
+    }
 }
