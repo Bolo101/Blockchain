f4a65cc666a0a307abdb6bdadd22a9978ddd6310
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-07-03 21:48:26 +0200

    Fix Sepolia chainId

--- ultimateLearning/foundryFundamentals/foundryLottery/script/HelperConfig.s.sol
+++ ultimateLearning/foundryFundamentals/foundryLottery/script/HelperConfig.s.sol
@@ -13,7 +13,7 @@ abstract contract CodeConstants {
     // Link/ETH price
     int256 public MOCK_WEI_PER_UINT_LINK = 4e15;
 
-    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 1115511;
+    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
 
@@ -37,7 +37,9 @@ contract HelperConfig is Script, CodeConstants {
         networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
     }
 
-    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
+    function getConfigByChainId(
+        uint256 chainId
+    ) public returns (NetworkConfig memory) {
         if (networkConfigs[chainId].vrfCoordinator != address(0)) {
             return networkConfigs[chainId];
         } else if (chainId == LOCAL_CHAIN_ID) {
@@ -52,15 +54,16 @@ contract HelperConfig is Script, CodeConstants {
     }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
-        return NetworkConfig({
-            entranceFee: 0.01 ether,
-            interval: 30,
-            vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
-            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
-            callbackGasLimit: 500000,
-            subscriptionId: 54630111271625716154304862105252371786777757451988907283279648075240746655396,
-            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
-        });
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+                gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+                callbackGasLimit: 500000,
+                subscriptionId: 54630111271625716154304862105252371786777757451988907283279648075240746655396,
+                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
+            });
     }
 
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
@@ -69,8 +72,11 @@ contract HelperConfig is Script, CodeConstants {
         }
         // Deploy mocks
         vm.startBroadcast();
-        VRFCoordinatorV2_5Mock vrfCoordinatorMock =
-            new VRFCoordinatorV2_5Mock(MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK, MOCK_WEI_PER_UINT_LINK);
+        VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock(
+            MOCK_BASE_FEE,
+            MOCK_GAS_PRICE_LINK,
+            MOCK_WEI_PER_UINT_LINK
+        );
         LinkToken linkToken = new LinkToken();
         vm.stopBroadcast();
 
