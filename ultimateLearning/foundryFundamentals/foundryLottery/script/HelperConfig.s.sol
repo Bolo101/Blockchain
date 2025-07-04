7ec5cad8592003a2fbd4e61c2de1da68842a1a86
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-07-05 00:35:54 +0200

    Added account data for vm.startBroadcast signature

--- ultimateLearning/foundryFundamentals/foundryLottery/script/HelperConfig.s.sol
+++ ultimateLearning/foundryFundamentals/foundryLottery/script/HelperConfig.s.sol
@@ -28,6 +28,7 @@ contract HelperConfig is Script, CodeConstants {
         uint32 callbackGasLimit;
         uint256 subscriptionId;
         address link;
+        address account;
     }
 
     NetworkConfig public localNetworkConfig;
@@ -62,7 +63,8 @@ contract HelperConfig is Script, CodeConstants {
                 gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
                 callbackGasLimit: 500000,
                 subscriptionId: 54630111271625716154304862105252371786777757451988907283279648075240746655396,
-                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
+                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+                account: 0x1570EE0caA6570C534123b1bd5bC52abD7D0b877
             });
     }
 
@@ -87,7 +89,8 @@ contract HelperConfig is Script, CodeConstants {
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             callbackGasLimit: 500000,
             subscriptionId: 0,
-            link: address(linkToken)
+            link: address(linkToken),
+            account: 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38
         });
         return localNetworkConfig;
     }
