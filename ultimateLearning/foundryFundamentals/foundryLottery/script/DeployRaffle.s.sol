ad56f07303edc3a3b10f5e1d8fe663eb09b1a0e5
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-07-05 00:37:37 +0200

    Added signature to vm.startBroadcast

--- ultimateLearning/foundryFundamentals/foundryLottery/script/DeployRaffle.s.sol
+++ ultimateLearning/foundryFundamentals/foundryLottery/script/DeployRaffle.s.sol
@@ -18,15 +18,20 @@ contract DeployRaffle is Script {
 
         if (config.subscriptionId == 0) {
             CreateSubscription createSubscription = new CreateSubscription();
-            (config.subscriptionId, config.vrfCoordinator) =
-                createSubscription.createSubscription(config.vrfCoordinator);
+            (config.subscriptionId, config.vrfCoordinator) = createSubscription
+                .createSubscription(config.vrfCoordinator, config.account);
 
             //Fund
             FundSubscription fundSubscription = new FundSubscription();
-            fundSubscription.fundSubscription(config.vrfCoordinator, config.subscriptionId, config.link);
+            fundSubscription.fundSubscription(
+                config.vrfCoordinator,
+                config.subscriptionId,
+                config.link,
+                config.account
+            );
         }
 
-        vm.startBroadcast();
+        vm.startBroadcast(config.account);
         Raffle raffle = new Raffle(
             config.entranceFee,
             config.interval,
@@ -39,7 +44,12 @@ contract DeployRaffle is Script {
 
         AddConsumer addConsumer = new AddConsumer();
         // No broadcast as its done in addConsumer() function
-        addConsumer.addConsumer(address(raffle), config.vrfCoordinator, config.subscriptionId);
+        addConsumer.addConsumer(
+            address(raffle),
+            config.vrfCoordinator,
+            config.subscriptionId,
+            config.account
+        );
 
         return (raffle, helperConfig);
     }
