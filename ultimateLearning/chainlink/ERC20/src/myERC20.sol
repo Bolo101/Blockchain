b7f0df8fcdfc7f89dbb2c01b9d8a33514561db27
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-07-23 20:39:40 +0200

    Granting Role

--- ultimateLearning/chainlink/ERC20/src/myERC20.sol
+++ ultimateLearning/chainlink/ERC20/src/myERC20.sol
@@ -3,11 +3,17 @@
 pragma solidity 0.8.19;
 
 import {ERC20} from "@openzeppelin/contracts@5.4.0/token/ERC20/ERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts@5.4.0/access/AccessControl.sol";
 
-contract MyERC20 is ERC20 {
-    constructor() ERC20("Bolo Token", "BOL") {}
+contract MyERC20 is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    function mint(address to, uint256 amount) public {
+    constructor() ERC20("Bolo Token", "BOL") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 }
