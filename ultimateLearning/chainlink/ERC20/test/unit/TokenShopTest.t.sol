d99491c4c9cc289a01cffe840d4d56606111c976
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-08-02 16:08:50 +0200

    First test for TokenShop deployment to check granted MINTER_ROLE

---
+++ ultimateLearning/chainlink/ERC20/test/unit/TokenShopTest.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployTokenShop} from "script/DeployTokenShop.s.sol";
+import {BoloToken} from "src/MyERC20.sol";
+import {TokenShop} from "src/TokenShop.sol";
+import {AccessControl} from "@openzeppelin/contracts@5.4.0/access/AccessControl.sol";
+
+contract TokenShopTest is Test, AccessControl {
+    TokenShop tokenShop;
+    BoloToken token;
+    address USER = makeAddr("user");
+    uint256 constant STARTING_BALANCE = 10 ether;
+
+    function setUp() external {
+        DeployTokenShop deployTokenShop = new DeployTokenShop();
+        (token, tokenShop) = deployTokenShop.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testMinterRoleAttribution() public view {
+        // Test that TokenShop has the MINTER_ROLE using hasRole
+        bool hasRole = token.hasRole(token.MINTER_ROLE(), address(tokenShop));
+        assertTrue(hasRole);
+    }
+}
