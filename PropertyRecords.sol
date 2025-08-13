785b4be26f037f23c0602f1d0e59a7b06a367011
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-08-13 21:34:22 +0200

    Added license

--- PropertyRecords.sol
+++ PropertyRecords.sol
@@ -1,60 +1,87 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.7;
 
-contract Owner{
+contract Owner {
     address owner;
 
-    constructor(){
-        owner = msg.sender;//deployer of the contract is the owner
+    constructor() {
+        owner = msg.sender; //deployer of the contract is the owner
     }
 
-    modifier isOwner(){
-        require(msg.sender == owner,"Not the owner");//condition setting the modifier to execute targeted functions
+    modifier isOwner() {
+        require(msg.sender == owner, "Not the owner"); //condition setting the modifier to execute targeted functions
         _;
     }
 }
-contract FlatManagement is Owner{
 
-    uint counter;//manual id setter
+contract FlatManagement is Owner {
+    uint counter; //manual id setter
 
-    enum propertyType{field,flat,house}//kind of estate
+    enum propertyType {
+        field,
+        flat,
+        house
+    } //kind of estate
 
-    struct property{
-        uint id;//counter 
+    struct property {
+        uint id; //counter
         string name;
         uint price;
         propertyType _propertyType;
     }
 
-    mapping(address => property[]) Assets;//collection of property struct arrays linked to an address
+    mapping(address => property[]) Assets; //collection of property struct arrays linked to an address
 
-    function addRealEstate(address _owner,string memory _name, uint _price, propertyType _propertyType) public isOwner{//only owner can execute
-        require(uint(_propertyType) >= 0,"Not in the range, too low");//property type verification
-        require(uint(_propertyType) <=2,">Not in the range, too high");
-        Assets[_owner].push(property(counter,_name,_price,_propertyType));//property added to the array collection of targeted address
-        counter ++;
+    function addRealEstate(
+        address _owner,
+        string memory _name,
+        uint _price,
+        propertyType _propertyType
+    ) public isOwner {
+        //only owner can execute
+        require(uint(_propertyType) >= 0, "Not in the range, too low"); //property type verification
+        require(uint(_propertyType) <= 2, ">Not in the range, too high");
+        Assets[_owner].push(property(counter, _name, _price, _propertyType)); //property added to the array collection of targeted address
+        counter++;
     }
 
-    function getProperties(address _owner) public view isOwner returns(property[] memory){//returns all data for a specific address
+    function getProperties(
+        address _owner
+    ) public view isOwner returns (property[] memory) {
+        //returns all data for a specific address
         return Assets[_owner];
     }
 
-    function getPropertiesByID(uint _index, address _owner) public view isOwner returns(property memory){//return data for a specific ID
-        property[] memory properties = Assets[_owner];//get data in memory struct 'properties'
-        for(uint i = 0; i < properties.length; i++){//read data
-            if(properties[i].id == _index){//check condition
-                return properties[i];// if condition matched, result is returned
+    function getPropertiesByID(
+        uint _index,
+        address _owner
+    ) public view isOwner returns (property memory) {
+        //return data for a specific ID
+        property[] memory properties = Assets[_owner]; //get data in memory struct 'properties'
+        for (uint i = 0; i < properties.length; i++) {
+            //read data
+            if (properties[i].id == _index) {
+                //check condition
+                return properties[i]; // if condition matched, result is returned
             }
-        } 
+        }
         revert("Unknown ID");
     }
-    function getPropertiesByName(string memory _name, address _owner) public view isOwner returns(property memory){
+
+    function getPropertiesByName(
+        string memory _name,
+        address _owner
+    ) public view isOwner returns (property memory) {
         property[] memory properties = Assets[_owner];
-        for(uint i = 0; i < properties.length; i++){
-            if(keccak256(bytes(properties[i].name)) == keccak256(bytes(_name))){//can not check strings with == ; can not compare string with == operator, byte error returned so converted data in bytes before hash function to check the output 
+        for (uint i = 0; i < properties.length; i++) {
+            if (
+                keccak256(bytes(properties[i].name)) == keccak256(bytes(_name))
+            ) {
+                //can not check strings with == ; can not compare string with == operator, byte error returned so converted data in bytes before hash function to check the output
                 return properties[i];
             }
-        } 
+        }
         revert("Unknown ID");
     }
-    
 }
