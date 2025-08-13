89a091f5aec3a6286677cd0b0f092c31714d295a
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-08-13 20:55:31 +0200

    Updated scripts file name

--- ultimateLearning/chainlink/Automation/script/DeployCustomLogic.s.sol
+++ ultimateLearning/chainlink/Automation/script/DeployCustomLogic.s.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 import {Script} from "forge-std/Script.sol";
 import {CustomLogic} from "../src/CustomLogic.sol";
 
-contract DeployTimeBased is Script {
+contract DeployCustomLogic is Script {
     function run() external {
         vm.startBroadcast();
         new CustomLogic(100);
