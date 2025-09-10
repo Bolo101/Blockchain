768a8c8a23073f8c00cf9e9766fe2c18040487fd
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-09-10 21:25:01 +0200

    Release version added

--- ultimateLearning/chainlink/CCIP/src/chainlink-local/CCIPLocalSimulator.sol
+++ ultimateLearning/chainlink/CCIP/src/chainlink-local/CCIPLocalSimulator.sol
@@ -3,4 +3,4 @@
 pragma solidity 0.8.20;
 
 // solhint-disable no-unused-import
-import {CCIPLocalSimulator} from "@chainlink/local/src/ccip/CCIPLocalSimulator.sol";
+import {CCIPLocalSimulator} from "@chainlink/local@0.2.5/src/ccip/CCIPLocalSimulator.sol";
