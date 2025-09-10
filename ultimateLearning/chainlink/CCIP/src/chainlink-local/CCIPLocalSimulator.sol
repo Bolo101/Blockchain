42eadb9546f9eecfcc2a252b6e4ebacd35046ba5
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-09-10 21:22:49 +0200

    Chainlink-local helper contract

---
+++ ultimateLearning/chainlink/CCIP/src/chainlink-local/CCIPLocalSimulator.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+// solhint-disable no-unused-import
+import {CCIPLocalSimulator} from "@chainlink/local/src/ccip/CCIPLocalSimulator.sol";
